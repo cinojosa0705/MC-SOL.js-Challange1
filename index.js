adce636e9a594aa13a1e2024889e109ff8b4ee6e
Carlos Inoiosa <carlito.inojosa@Carloss-MBP.attlocal.net>
2022-10-08 13:59:15 -0400

    cleaning up

--- index.js
+++ index.js
@@ -3,50 +3,19 @@ const {
     Connection,
     PublicKey,
     clusterApiUrl,
-    Keypair,
     LAMPORTS_PER_SOL
 } = require("@solana/web3.js");
-
-// Create a new keypair
-const newPair = new Keypair();
-
-// Exact the public and private key from the keypair
-const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
-const privateKey = newPair._keypair.secretKey;
-
-// Connect to the Devnet
-const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-// console.log("Public Key of the generated keypair", publicKey);
-
-// Get the wallet balance from a given private key
-const getWalletBalance = async () => {
-    try {
-        // Connect to the Devnet
-        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        console.log("Connection object is:", connection);
-
-        // Make a wallet (keypair) from privateKey and get its balance
-        const myWallet = await Keypair.fromSecretKey(privateKey);
-        const walletBalance = await connection.getBalance(
-            new PublicKey(newPair.publicKey)
-        );
-        console.log(`Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL`);
-    } catch (err) {
-        console.log(err);
-    }
-};
 
 const airDropSol = async () => {
     try {
         // Connect to the Devnet and make a wallet from privateKey
         const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-        const myWallet = await Keypair.fromSecretKey(privateKey);
 
         // Request airdrop of 2 SOL to the wallet
         console.log("Airdropping some SOL to your wallet!");
         const fromAirDropSignature = await connection.requestAirdrop(
             new PublicKey(process.argv.slice(2)[0]),
+            //            ^^^^^^^^^^^^^^^^^^^^^^^^ Here we get the users public key from the CLI
             2 * LAMPORTS_PER_SOL
         );
         await connection.confirmTransaction(fromAirDropSignature);
@@ -55,11 +24,11 @@ const airDropSol = async () => {
     }
 };
 
-// Show the wallet balance before and after airdropping SOL
+// Gets users wallet adress from CLI input and airdrops Devnet SOL to them
 const mainFunction = async () => {
-    // await getWalletBalance();
-     await airDropSol();
-    // await getWalletBalance();
+    
+    await airDropSol();
+
     console.log("You've succesfully Airdropped 2 Devnet SOL to your wallet!")
 }
 
